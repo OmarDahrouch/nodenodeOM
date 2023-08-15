398194f635cfe60f0f2af6ae9473b5505ae55597
OmarDahrouch <idahrouch2@gmail.com>
2023-08-15 12:27:43 +0100

    Environement 2

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Sequelize } = require("sequelize");
+// const userRoute = require("./routes/userRoute");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3030;
+
+app.use(cors());
+
+// DataBase Config ----------------------------------------------------
+const db = require("./models/index");
+
+db.sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+app.use(express.json());
+
+// app.use("/users", userRoute);
